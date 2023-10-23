153b7e94cb205e42c30e10b46630c958f3b37f40
aminwoo <ben.woo.careers@gmail.com>
2023-10-24 00:16:10 +1100

    change formatting of ant question

--- txtFiles/math.ts
+++ txtFiles/math.ts
@@ -1,7 +1,11 @@
 const question = 
-`Mr Ant moves on the Cartesian plane according to the following rules:
+`
+Mr Ant moves on the Cartesian plane according to the following rules:
+
     1. From any lattice point (a,b), the ant may only move to (a+1,b), (a,b+1), or (a+1,b+1).
     2. There are no right angle turns in the ant's path.
+
 Help Mr Ant calculate the number of different paths from the origin to the point (5,5).
+
 `
 export default question; 
